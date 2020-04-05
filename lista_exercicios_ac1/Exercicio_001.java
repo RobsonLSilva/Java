6860e1acaa71b7a85d70c994a3503e1adddca212
Robson Luiz <robson.lsilva36@gmail.com>
2020-04-04 22:11:15 -0300

    Correção 1º Exercicío Java

--- lista_exercicios_ac1/Exercicio_001.java
+++ lista_exercicios_ac1/Exercicio_001.java
@@ -6,20 +6,18 @@ public class Exercicio_001 {
 
     public static void main(final String[] args) {
 
-        int n1 = 0;
-        int n2 = 0;
-        int result = 0;
-
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Digite o primeiro numero:");
-        n1 = sc.nextInt();
+        int n1 = sc.nextInt();
 
         System.out.println("Digite o primeiro numero:");
-        n2 = sc.nextInt();
+        int n2 = sc.nextInt();
 
-        result = n1 + n2;
-        System.out.println("O resultado da soma é "+result);        
+        int result = n1 + n2;
+        System.out.println("O resultado da soma é "+result);
+        
+        sc.close();
         
         }
         
